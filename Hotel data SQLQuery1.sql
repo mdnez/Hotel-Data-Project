a61e41e96109551d74c97510b4c1d81935116154
mdnez <142688943+mdnez@users.noreply.github.com>
2023-11-01 17:47:29 -0700

    Project process

---
+++ Hotel data SQLQuery1.sql
@@ -0,0 +1,13 @@
+
+with Hotels as (
+select * from dbo.['2018$']
+union
+select * from dbo.['2019$']
+union
+select * from dbo.['2020$'])
+
+Select * from Hotels
+left join dbo.market_segment$
+on hotels.market_segment = market_segment$.market_segment
+left join dbo.meal_cost$
+on meal_cost$.meal =Hotels.meal
